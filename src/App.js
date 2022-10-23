db325fbfb4b3991e08532866d4b59182699fc77a
unknown <roysatyajit772912@gmail.com>
2022-10-23 22:14:49 +0530

    jkv

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import './App.css';
-import AboutUs from './components/AboutUs';
-import Navbar from './components/Navbar';
+// import AboutUs from './components/AboutUs';
+// import Navbar from './components/Navbar';
 // import TextFrom from './components/TextForm';
 
 function App() {
   return (
     <>
-        <Navbar title = "Navbar" Pricing ="High Pricing" features='Features'/>
+        {/* <Navbar title = "Navbar" Pricing ="High Pricing" features='Features'/>
         <div className='container my-3'>
        <AboutUs/>
-        </div>
+        </div> */} 
           
     </>
 
