37fb6d0816b4c2c68b973f270f1ecf4d53092426
unknown <roysatyajit772912@gmail.com>
2022-10-25 19:24:52 +0530

    jh

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import './App.css';
 function App() {
   return (
     <>
-        {/* <Navbar title = "Navbar" Pricing ="High Pricing" features='Features'/>
+         <Navbar title = "Navbar" Pricing ="High Pricing" features='Features'/>
         <div className='container my-3'>
        <AboutUs/>
-        </div> */} 
+        </div> 
+
+
           
     </>
 
